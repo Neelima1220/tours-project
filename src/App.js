7578762b8ea54663dad45fcc0700637c4365c697
Neelima1220 <95346804+Neelima1220@users.noreply.github.com>
2022-09-13 21:20:01 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { cloneDeep } from 'lodash';
 import './style.css';
-import chalk from 'chalk';
-// const chalk = require('chalk');
+
 export default function App() {
-  console.log(chalk.blue('Hello world!'));
+  const url = 'https://course-api.com/react-tours-project';
+
+  const [data, setData] = useState();
+  const [showMore, setShowMore] = useState(false);
+
+  const fetchData = async () => {
+    const response = await (await fetch(url)).json();
+    console.log(response);
+    setData(response);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRemove = (i) => {
+    const tempData = cloneDeep(data);
+    const newData = tempData.filter((item, index) => {
+      return index !== i;
+    });
+    console.log(tempData, newData);
+    setData(newData);
+  };
+  const clearAll = () => {
+    setData({});
+  };
+  const handleShowMore = (i) => {
+    const tempData = cloneDeep(data);
+    const newData = tempData.map((item, index) => {
+      if (index === i) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
+    });
+    // setShowMore(!showMore);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={clearAll}>clear All</button>
+      {data?.length > 0 &&
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <h4>{item.name}</h4>
+              <p>{item.info.slice(0, 99)}</p>
+              <button onClick={() => handleShowMore(index)}>
+                {showMore ? 'show less' : 'show more'}
+              </button>
+              {showMore && <p>{item.info.slice(99, item.info.length)}</p>}
+              <span>{item.price}</span>
+              <button onClick={() => handleRemove(index)}>remove</button>
+            </div>
+          );
+        })}
     </div>
   );
 }
